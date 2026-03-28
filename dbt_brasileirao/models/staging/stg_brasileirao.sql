5ff2ccd295ecbce6758900a6485947ed8ede2808
jhonatangs <jhonatangs1@gmail.com>
2026-03-28 03:59:00 -0300

    feat: staging brasileirao dbt layer

---
+++ dbt_brasileirao/models/staging/stg_brasileirao.sql
@@ -0,0 +1,19 @@
+{{ config(materialized='view') }}
+
+with raw_data as (
+    select *
+    from read_parquet('../data/raw/brasileirao_2026.parquet')
+),
+
+casted as (
+    select
+        cast(time as varchar) as time,
+        cast(jogos as integer) as jogos,
+        cast(vitorias as integer) as vitorias,
+        cast(empates as integer) as empates,
+        cast(derrotas as integer) as derrotas,
+        cast(pontos as integer) as pontos
+    from raw_data
+)
+
+select * from casted

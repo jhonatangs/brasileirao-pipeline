6a6aef706169895859f919b7990037dfe0c9f30b
jhonatangs <jhonatangs1@gmail.com>
2026-03-28 20:28:57 -0300

    feat: extract matches airflow task

--- dags/extract_brasileirao.py
+++ dags/extract_brasileirao.py
@@ -17,11 +17,18 @@ def extract_brasileirao():
 
     extract_standings_task = extract_standings()
 
+    @task
+    def extract_matches():
+        from include.scrapers.br_scraper import scrape_brasileirao_matches
+        scrape_brasileirao_matches()
+
+    extract_matches_task = extract_matches()
+
     dbt_build = BashOperator(
         task_id="dbt_build",
         bash_command="dbt build --project-dir /usr/local/airflow/dbt_brasileirao --profiles-dir /usr/local/airflow/dbt_brasileirao"
     )
 
-    extract_standings_task >> dbt_build
+    [extract_standings_task, extract_matches_task] >> dbt_build
 
 extract_brasileirao()

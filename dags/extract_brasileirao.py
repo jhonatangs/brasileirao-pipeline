8e438ffc350f740d32796246e389a88e1f1b4c58
jhonatangs <jhonatangs1@gmail.com>
2026-03-29 04:28:55 -0300

    feature: scrape future matches

--- dags/extract_brasileirao.py
+++ dags/extract_brasileirao.py
@@ -24,11 +24,18 @@ def extract_brasileirao():
 
     extract_matches_task = extract_matches()
 
+    @task
+    def extract_future_matches():
+        from include.scrapers.br_scraper import scrape_brasileirao_future_matches
+        scrape_brasileirao_future_matches()
+
+    extract_future_matches_task = extract_future_matches()
+
     dbt_build = BashOperator(
         task_id="dbt_build",
         bash_command="dbt build --project-dir /usr/local/airflow/dbt_brasileirao --profiles-dir /usr/local/airflow/dbt_brasileirao"
     )
 
-    [extract_standings_task, extract_matches_task] >> dbt_build
+    [extract_standings_task, extract_matches_task, extract_future_matches_task] >> dbt_build
 
 extract_brasileirao()
